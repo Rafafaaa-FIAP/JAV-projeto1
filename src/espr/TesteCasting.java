83f414420ab5c1e979a95d67f55d6b54505c37e2
Rafafaaa <cristofalirafael@gmail.com>
2024-08-28 21:02:26 -0300

    aula 28-08-2024

--- src/espr/TesteCasting.java
+++ src/espr/TesteCasting.java
@@ -1,11 +1,21 @@
 package espr;
 
 public class TesteCasting {
+	
+	public static void main(String[] args) {
+		
+		double variavel = 10.9;
+		
+		long valorLongo = 123456789879776868l;
+		
+		int inteiro = (int)variavel;
+		
+		int inteiro2 = (int)valorLongo;
+		
+		System.out.println(inteiro);
+		
+		System.out.println(inteiro2);
+		
+	}
 
-	/*long valorLong = 1234567898798768681;
-	
-	int inteiro = (int)variavel;
-	
-	int inteiro2 = (int)valorLong;*/
-	
 }
